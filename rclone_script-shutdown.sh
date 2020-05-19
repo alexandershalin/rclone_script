61aa1b704fc067ada6fa738feebced3d607b8951
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2020-05-19 10:02:41 -0700

    fix the logfile so that it all logs in the one file

--- rclone_script-shutdown.sh
+++ rclone_script-shutdown.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-logfile=/home/pi/scripts/rclone_script/shutdown.log
 source /home/pi/scripts/rclone_script/rclone_script.ini
 source /home/pi/scripts/rclone_script/rclone_script-fns.sh
 
@@ -10,8 +9,8 @@ source /home/pi/scripts/rclone_script/rclone_script-fns.sh
 
 if [ "${syncOnSystemStartStop}" == "TRUE" ]
 then
-	printf "$(date +%FT%T%:z):syncOnSystemStartStop is enabled, uploading savegames" >> "${logfile}"
+	printf "$(date +%FT%T%:z):syncOnSystemStartStop is enabled, uploading savegames\n" >> "${logfile}"
 	doUpSync >> "${logfile}" 2>&1
 else
-	printf "$(date +%FT%T%:z):syncOnSystemStartStop is disabled, skipping savegame upload" >> "${logfile}"
+	printf "$(date +%FT%T%:z):syncOnSystemStartStop is disabled, skipping savegame upload\n" >> "${logfile}"
 fi
--- rclone_script-startup.sh
+++ rclone_script-startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-logfile=/home/pi/scripts/rclone_script/startup.log
 source /home/pi/scripts/rclone_script/rclone_script.ini
 source /home/pi/scripts/rclone_script/rclone_script-fns.sh
 
@@ -10,10 +9,10 @@ source /home/pi/scripts/rclone_script/rclone_script-fns.sh
 
 if [ "${syncOnSystemStartStop}" == "TRUE" ]
 then
-	printf "$(date +%FT%T%:z):syncOnSystemStartStop is enabled, downloading savegames" >> "${logfile}"
-	printf "$(date +%FT%T%:z):sleep for 10s to allow load to complete" >> "${logfile}"
+	printf "$(date +%FT%T%:z):syncOnSystemStartStop is enabled, downloading savegames\n" >> "${logfile}"
+	printf "$(date +%FT%T%:z):sleep for 10s to allow load to complete\n" >> "${logfile}"
 	sleep 10; # put in a sleep to allow emulationstation to finish loading
 	doDownSync >> "${logfile}" 2>&1
 else
-	printf "$(date +%FT%T%:z):syncOnSystemStartStop is disabled, skipping savegame download" >> "${logfile}"
+	printf "$(date +%FT%T%:z):syncOnSystemStartStop is disabled, skipping savegame download\n" >> "${logfile}"
 fi
